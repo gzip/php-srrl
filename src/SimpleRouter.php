e1e2eb6b41f59fb0d5ed797e86e0a4977eb7378d
Gamaiel Zavala <gamaiel@yahoo.com>
2024-08-23 21:50:35 -0600

    Add continue option to allow multiple handlers for the same route

--- src/SimpleRouter.php
+++ src/SimpleRouter.php
@@ -8,15 +8,15 @@ class SimpleRouter extends SimpleClass
      * @var array
      */
     protected $routes = array();
-    
+
     /**
      * @var string
      */
     protected $verb = 'GET';
-    
+
     /**
      * Called by SimpleClass::__construct prior to setParams.
-     * 
+     *
      * @return void
      **/
     public function setupParams()
@@ -24,18 +24,18 @@ class SimpleRouter extends SimpleClass
         $this->addPushable('routes');
         $this->addSetter('routes', 'setRoute');
     }
-    
+
     protected function setRoute($route)
     {
         return is_array($route) && isset($route['route']) ? $route : null;
     }
-    
+
     public function addRoute($route)
     {
         $this->setOptions($route);
         $routeVal = $this->getOption('route');
         $type = $this->getOption('type', 'static');
-        
+
         // handle /:foo/:bar syntax
         if($type === 'static' && strpos($routeVal, ':'))
         {
@@ -48,16 +48,16 @@ class SimpleRouter extends SimpleClass
                     $routeVal = str_replace('/:'.$match[1], '/([^/]+)', $routeVal);
                     $matchKeys[] = $match[1];
                 }
-                
+
                 $route['type'] = 'regex';
                 $route['route'] = $routeVal;
                 $route['matchKeys'] = $matchKeys;
             }
         }
-        
+
         $this->pushRoute($route);
     }
-    
+
     public function addRoutes($routes)
     {
         if(is_array($routes))
@@ -67,15 +67,15 @@ class SimpleRouter extends SimpleClass
             }
         }
     }
-    
+
     public function route($path = null)
     {
         $this->verb = $_SERVER['REQUEST_METHOD'];
-        
+
         if(is_null($path)){
             $path = $this->getPath();
         }
-        
+
         $found = false;
         $accepts = null;
         foreach($this->routes as $route)
@@ -83,11 +83,15 @@ class SimpleRouter extends SimpleClass
             $this->setOptions($route);
             $type = $this->getOption('type', 'static');
             $value = $this->getOption('route', null);
-            
+
             $verbs = $this->getOption('verbs', array($this->getOption('verb', 'GET')));
             $accepted = $verbs === '*' || in_array($this->verb, $verbs);
             $error = $accepted ? null : 405;
-            
+
+            if ($error && $this->getOption('continue', false)) {
+                continue;
+            }
+
             $accept = SimpleUtil::arrayVal($this->getOption('accept'));
             if($accept && $accepted)
             {
@@ -98,12 +102,12 @@ class SimpleRouter extends SimpleClass
                 $accepted = count(array_diff($accept, $accepts));
                 $error = $accepted ? null : 406;
             }
-            
+
             $args = array_merge(
                 $this->getOption('args', array()),
                 array('verb'=>$this->verb, 'verbs'=>$verbs, 'route'=>$value, 'path'=>$path)
             );
-            
+
             switch($type)
             {
                 case 'static':
@@ -130,16 +134,16 @@ class SimpleRouter extends SimpleClass
                     $this->log('Unknown route type "'.$type.'".');
                 break;
             }
-            
+
             if($found)
             {
                 return $error ? SimpleHttp::error($error) : $this->handleRoute($route, $args);
             }
         }
-        
+
         return false;
     }
-    
+
     public function handleRoute($route, $args = array())
     {
         $this->setOptions($route);
@@ -148,14 +152,14 @@ class SimpleRouter extends SimpleClass
         if($method)
         {
             $obj = null;
-            
+
             if( is_array($method) && 2 === count($method) &&
                 is_object($method[0]) && is_string($method[1])){
                 list($obj, $method) = $method;
             } else if (is_string($method)) {
                 $obj = $this;
             }
-            
+
             if($obj && is_callable( array($obj, $method) )){
                 return $obj->$method($args);
             } else {
@@ -176,7 +180,7 @@ class SimpleRouter extends SimpleClass
             $this->log('Unable to route "'.$path.'".');
         }
     }
-    
+
     public function getPath()
     {
         return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
