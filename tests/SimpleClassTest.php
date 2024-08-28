867fe4479b5e07b3c024dad709689d1869e2849d
Gamaiel Zavala <gamaiel@yahoo.com>
2024-08-27 20:01:26 -0600

    Full coverage for SimpleClass

--- tests/SimpleClassTest.php
+++ tests/SimpleClassTest.php
@@ -7,7 +7,7 @@ class SimpleClassTest extends PHPUnit\Framework\TestCase
     /**
      * @var SimpleClass
      */
-    protected $object;
+    protected $proxy;
 
     /**
      * Sets up the fixture, for example, opens a network connection.
@@ -15,7 +15,7 @@ class SimpleClassTest extends PHPUnit\Framework\TestCase
      */
     protected function setUp() : void
     {
-        $this->object = new SimpleClass;
+        $this->proxy = new SimpleClassProxy;
     }
 
     /**
@@ -26,70 +26,215 @@ class SimpleClassTest extends PHPUnit\Framework\TestCase
     {
     }
 
-    /**
-     * @todo Implement test__call().
-     */
+    public function readLog($msg)
+    {
+        $this->assertEquals($msg, array_pop($this->proxy->logs));
+    }
+
     public function test__call()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
+        $this->assertFalse($this->proxy->hasTestBool());
+        $this->assertFalse($this->proxy->isTestBool());
+
+        $this->proxy->enableTestBool();
+        $this->assertTrue($this->proxy->isTestBool());
+
+        $this->proxy->disableTestBool();
+        $this->assertFalse($this->proxy->isTestBool());
+
+        // property doesn't exist
+        $this->assertNull($this->proxy->getTestDoesntExist());
+
+        // property exists but is not gettable
+        $this->assertNull($this->proxy->getTestNope());
+        $this->proxy->setTestNope('bar');
+        $this->assertNull($this->proxy->getTestNope());
+
+        // gettable but not settable
+        $this->proxy->setTestSet('baz');
+        $this->assertEquals('test', $this->proxy->getTestSet());
+
+        // setter is for string so it should ignore int
+        $this->proxy->setTestStr(5);
+        $this->assertEquals('test', $this->proxy->getTestStr());
+        $this->readLog('Ignoring invalid value for testStr: 5');
+
+        // normal set on a string
+        $this->proxy->setTestStr('foo');
+        $this->assertEquals('foo', $this->proxy->getTestStr());
+
+        // enabling a non-boolean
+        $this->proxy->enableTestStr();
+        $this->assertEquals('foo', $this->proxy->getTestStr());
+        $this->readLog('Ignoring enable/disable for parameter testStr which is not a boolean.');
+
+        $this->proxy->addTestArray('foo');
+        $this->assertEquals(array('foo'), $this->proxy->getTestArray());
+        $this->proxy->pushTestArray('bar');
+
+        // test set branch
+        $this->assertEquals(array('foo', 'bar'), $this->proxy->getTestArray());
+        $this->proxy->setTestArray('bat');
+        $this->assertEquals(array('foo', 'bar', 'bat'), $this->proxy->getTestArray());
+
+        // test set of pushable
+        $this->proxy->setTestArray(array('baz'));
+        $this->assertEquals(array('baz'), $this->proxy->getTestArray());
+
+        $this->proxy->fooBar();
+        $this->readLog('Unknown method: SimpleClassProxy->fooBar');
     }
 
-    /**
-     * @todo Implement testSetup().
-     */
+    public function testParseAction()
+    {
+        $this->assertEquals(array('bar', 'foo'), $this->proxy->parseActionProxy("fooBar"));
+        $this->assertEquals(array('bar', ''), $this->proxy->parseActionProxy("Bar"));
+        $this->assertEquals(array('fooBar', ''), $this->proxy->parseActionProxy("FooBar"));
+    }
+
     public function testSetup()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
+        $this->assertTrue($this->proxy->setup());
+        $this->assertNull($this->proxy->setupParams());
     }
 
-    /**
-     * @todo Implement testSetupParams().
-     */
-    public function testSetupParams()
+    public function testPush()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
+        // plural property branch
+        $this->proxy->pushTestDir(__DIR__);
+        $this->assertEquals(array(__DIR__), $this->proxy->getTestDirs());
+
+        // invalid value
+        $this->proxy->pushTestArray(false);
+        $this->readLog("Ignoring invalid value pushed to property 'testArray'.");
+
+        // unpushable
+        $this->proxy->pushTestStr(null);
+        $this->readLog("Property 'testStr' is not pushable.");
     }
 
-    /**
-     * @todo Implement testGet().
-     */
-    public function testGet()
+    public function testVerifyParameterValue()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
+        // invalid value in subarray
+        $this->proxy->pushTestArray(array('foo', array('bar', null, 'baz')));
+        $this->assertEquals(array('foo', array('bar', 'baz')), $this->proxy->getTestArray());
+        $this->readLog("Ignoring invalid value in testArray[1]: ");
     }
 
-    /**
-     * @todo Implement testSet().
-     */
-    public function testSet()
+    public function testAddSetter()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
+        $this->assertNull($this->proxy->addSetter('', function(){}));
+        $this->assertNull($this->proxy->addSetter(null, function(){}));
+
+        // 'foo' becomes $this->foo() which is callable through enter __call
+        $this->assertTrue($this->proxy->addSetter('testStr', 'foo'));
+        // but it will fail with null when executing validation
+        $this->assertNull($this->proxy->setTestStr('foo'));
+        $this->assertEquals('test', $this->proxy->getTestStr());
+
+        $this->assertNull($this->proxy->addSetter('foo', function(){}));
+        $this->readLog("Ignoring unresolvable setter function for foo.");
     }
 
-    /**
-     * @todo Implement testSetParams().
-     */
     public function testSetParams()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
+        $params = array('testBool'=>true, 'testStr'=>'foo', 'testArray'=>array('baz', 'quuz'), 'bar'=>'bar');
+        $this->proxy->setParams($params);
+        $this->readLog("Ignoring set for invalid property bar.");
+        $this->assertEquals($params['testStr'], $this->proxy->getTestStr());
+        $this->assertEquals($params['testBool'], $this->proxy->getTestBool());
+        $this->assertEquals($params['testArray'], $this->proxy->getTestArray());
+    }
+
+    public function testSetOptions()
+    {
+        $this->proxy->addGettable(array('options'));
+        $opts = array('one'=>1, 'two'=>2);
+        $this->proxy->setOptions($opts);
+        $this->assertEquals($opts, $this->proxy->getOptions());
+        $this->proxy->setOptions('foo');
+        $this->assertEquals($opts, $this->proxy->getOptions());
+
+        $this->assertEquals(1, $this->proxy->getOption('one'));
+        $this->assertEquals(4, $this->proxy->getOption('three', 4));
+    }
+
+    public function testResolveCallable()
+    {
+        $this->assertNull($this->proxy->resolveCallable(array()));
+        $this->assertEquals('is_callable', $this->proxy->resolveCallable('is_callable'));
+        $this->assertNull($this->proxy->resolveCallable('foo', false));
+    }
+
+    public function testLog()
+    {
+        $this->proxy->enableBacktrace();
+        $this->assertNull($this->proxy->log(array(), true));
     }
 }
-?>
+
+
+class SimpleClassProxy extends SimpleClass
+{
+    public    $logs      = array();
+    protected $testArray = array();
+    protected $testBool  = false;
+    protected $testDirs  = array();
+    protected $testStr   = 'test';
+    protected $testSet   = 'test';
+    protected $testNope  = 'nope';
+
+    public function setupParams()
+    {
+        // coverage
+        parent::setupParams();
+
+        $this->addPushable(array('logs', 'testArray', 'testDirs'));
+        $this->addSettable(array('testBool', 'testStr'));
+        $this->addGettable(array('testSet'));
+
+        $this->addSetter('testArray', 'setStringOrArray');
+        $this->addSetter('testBool',  'setBoolean');
+        $this->addSetter('testDirs',  'setDirectoryPath');
+        $this->addSetter('testStr',   'setString');
+    }
+
+    // expose publicly for testing
+    public function parseActionProxy($method)
+    {
+        return parent::parseAction($method);
+    }
+
+    // expose publicly for testing
+    public function addSetter($name, $func)
+    {
+        return parent::addSetter($name, $func);
+    }
+
+    // expose publicly for testing
+    public function setOptions($opts)
+    {
+        return parent::setOptions($opts);
+    }
+
+    // expose publicly for testing
+    public function getOption($name, $default = '', $allowEmpty = true)
+    {
+        return parent::getOption($name, $default, $allowEmpty);
+    }
+
+    // expose publicly for testing
+    public function resolveCallable($call, $resolveToThis = true)
+    {
+        return parent::resolveCallable($call, $resolveToThis);
+    }
+
+    public function log($msg, $parent = false)
+    {
+        if ($parent) {
+            parent::log($msg);
+        } else {
+            $this->logs[] = $msg;
+        }
+    }
+}
